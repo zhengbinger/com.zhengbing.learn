1c97a4babfd24a979b50981be5386d08aba7a2bf
AngularZheng <mydreambing@126.com>
2019-06-19 21:28:34 +0800

    remove jms  module

---
+++ com.zhenbing.learn.concurrence/src/main/java/com/zhengbing/learn/concurrence/package-info.java
@@ -0,0 +1,3 @@
+/** Created by zhengbing on 2019-06-19. Since Version 1.0 */
+/** 并发处理 */
+package com.zhengbing.learn.concurrence;
---
+++ com.zhengbing.learn.design/src/main/java/strategy/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created by zhengbing on 2019-06-19.
+ * Since Version 1.0
+ */
+/**
+ * 以商场收银的案例来了解策略模式的实现
+ * 策略模式：
+ */
+package strategy;
